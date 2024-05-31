8c91cda4abcddb9f5270d3f0eb1a70cca333930c
hqshe <defadein@gmail.com>
2024-05-31 09:18:18 +0300

    models added

--- gallery/models.py
+++ gallery/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
-class Category(models.model):
+class Category(models.Model):
     name = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
+
+class Image(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/')
+    categories = models.ManyToManyField('Category')
+    created_date = models.DateField(auto_now_add=True)
+    age_limit = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
