302e58945a08cca891e517a77246a9214b94fe60
hqshe <defadein@gmail.com>
2024-05-31 09:37:36 +0300

    category tests where added

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 class Category(models.Model):
@@ -12,6 +13,10 @@ class Image(models.Model):
     categories = models.ManyToManyField('Category')
     created_date = models.DateField(auto_now_add=True)
     age_limit = models.PositiveIntegerField()
+
+    def clean(self):
+        if self.age_limit < 0:
+            raise ValidationError('Age limit cannot be negative.')
 
     def __str__(self):
         return self.title
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+# gallery/tests.py
 
-# Create your tests here.
+from django.test import TestCase
+from django.core.exceptions import ValidationError
+from .models import Category, Image
+from django.utils import timezone
+
+class CategoryModelTest(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name='Test Category')
+
+    def test_category_creation(self):
+        self.assertEqual(self.category.name, 'Test Category')
+        self.assertEqual(str(self.category), 'Test Category')
+
+    def test_category_name_max_length(self):
+        max_length = self.category._meta.get_field('name').max_length
+        self.assertEqual(max_length, 20)
